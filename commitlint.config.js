4b9ab32a451b707ee0ec3f61dd4af08d0fbb8555
Pdhenrique <pedroschw.c@gmail.com>
2023-10-23 18:43:23 -0300

    feat(core): lint setup

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,24 @@
 /* global module */
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-      "type-enum": [
-          2,
-          "always",
-          ["feat", "fix", "chore", "docs", "style", "refactor", "test"],
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test'],
+    ],
+    'scope-empty': [2, 'never'],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'front-end',
+        'back-end',
+        'core',
       ],
-      "scope-empty": [2, "never"],
-      "scope-enum": [
-          2,
-          "always",
-          [
-            "front-end",
-            "back-end",
-            "core",
-          ],
-      ],
-      "subject-empty": [2, "never"],
-      "subject-full-stop": [2, "never", "."],
-      "subject-case": [2, "never", ["upper-case"]],
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'subject-case': [2, 'never', ['upper-case']],
   },
 }
