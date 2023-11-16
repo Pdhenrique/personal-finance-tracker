b6cf3d26de9f0ae419ec65da04be3a9d811044b7
Pedro Santana <pedroschw.c@gmail.com>
2023-11-16 12:42:17 -0300

    feat(front-end): add routes

--- FinancePal/src/App.tsx
+++ FinancePal/src/App.tsx
@@ -1,12 +1,18 @@
 import { RegisterForm } from "./components/RegisterForm"
+import DataTable from "./components/MUITable/table"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
 import "./App.css" //Temporário 
 
 function App() {
 
   return (
-    
-      <RegisterForm/>
-    
+    <Router>
+      <Routes>
+        <Route path="/login" element={<RegisterForm />} />
+        <Route path="/main" element={<DataTable/>} />
+      </Routes>
+    </Router>
   )
 }
 
