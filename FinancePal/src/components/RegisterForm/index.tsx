469c5bb420dea848860c8cff9ef9bd298be00c3c
Pedro Santana <pedro.ana@nexti.com>
2023-11-16 11:47:37 -0300

    refactor(front-end): changed form stylization

--- FinancePal/src/components/RegisterForm/index.tsx
+++ FinancePal/src/components/RegisterForm/index.tsx
@@ -1,52 +1,65 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import { Tab, Box, TextField } from "@mui/material";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { StyledContainer, StyledForm, StyledFormSection } from "./form.styles";
+import { Tab, Box } from "@mui/material"
+import { TabContext, TabList, TabPanel } from "@mui/lab"
+
+import "./styles.css"
 
 export const RegisterForm = () => {
-  const [value, setValue] = useState("1");
+  const [value, setValue] = useState("1")
 
   const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
+    setValue(newValue)
+  }
 
   return (
     <>
       <TabContext value={value}>
-        <StyledContainer>
-            <Box sx={{width: '600', marginLeft: '35px' }}>
-            <TabList  onChange={handleChange} aria-label="lab API tabs example">
-                <Tab sx={{backgroundColor: "#F8F9FA", borderTopLeftRadius: 5}} label="Register" value="1" />
-                <Tab sx={{backgroundColor: "#F8F9FA",  borderTopRightRadius: 5}} label="Login"    value="2" />
+        <div id="formContainer">
+          <Box sx={{ width: "fit-content", marginLeft: "35px" }}>
+            <TabList sx={{ boxShadow: "-10 0 5 black" }} onChange={handleChange} aria-label="lab API tabs example">
+              <Tab sx={{ backgroundColor: "#e6e6e69c", borderTopLeftRadius: 5 }} label="Register" value="1" />
+              <Tab sx={{ backgroundColor: "#e6e6e69c ", borderTopRightRadius: 5 }} label="Login" value="2" />
             </TabList>
-            </Box>
-            <TabPanel value="1">
-            <StyledForm>
-                <StyledFormSection>
-                <TextField
-                    label="outlined"
-                    variant="outlined"
-                    color="info"
-                    sx={{ width: "100%" }}
-                />
-                </StyledFormSection>
-            </StyledForm>
-            </TabPanel>
-            <TabPanel value="2">
-            <StyledForm>
-                <StyledFormSection>
-                <TextField
-                    label="outlined"
-                    variant="outlined"
-                    color="info"
-                    sx={{ width: "100%" }}
-                />
-                </StyledFormSection>
-            </StyledForm>
-            </TabPanel>
-        </StyledContainer>
+          </Box>
+          <TabPanel value="1">
+            <form id="register-form" className="user-form">
+              <div className="form-section">
+                <input type="email" placeholder="E-mail" />
+              </div>
+              <div className="form-section">
+                <input type="text" placeholder="Nome" />
+              </div>
+              <div className="form-section">
+                <input type="password" placeholder="Senha" />
+              </div>
+              <div className="form-section">
+                <input type="password" placeholder="Repita a senha" />
+              </div>
+              <div className="form-section">
+                <button type="submit">Button</button>
+                <p>forgot your password?</p>
+              </div>
+            </form>
+          </TabPanel>
+          <TabPanel value="2">
+            <form id="login-form" className="user-form">
+              <div className="form-section">
+                <input type="email" placeholder="E-mail" />
+              </div>
+              <div className="form-section">
+                <input type="password" placeholder="Repita a senha" />
+              </div>
+              <div className="form-section">
+                <button type="submit">Button</button>
+                <p>forgot your password?</p>
+              </div>
+            </form>
+
+          </TabPanel>
+        </div>
       </TabContext>
     </>
-  );
-};
+  )
+}
+
