f1638a3d6e7baf7323f086d0c0fdf99eb4189ab7
Pedro Santana <pedroschw.c@gmail.com>
2023-11-21 19:46:32 -0300

    feat(front-end): add register method

--- FinancePal/src/components/RegisterForm/index.tsx
+++ FinancePal/src/components/RegisterForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { MouseEvent, useState } from "react"
 
 import { Tab, Box } from "@mui/material"
 import { TabContext, TabList, TabPanel } from "@mui/lab"
@@ -6,15 +6,49 @@ import { TabContext, TabList, TabPanel } from "@mui/lab"
 import "./styles.css"
 
 export const RegisterForm = () => {
-  const [value, setValue] = useState("1")
+
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [repassword, setRepassword] = useState("")
+
+  const handleRegister = async (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault()
+    
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ 
+          username, password, email, repassword
+        }) 
+      })
+      if(password === repassword){
+        if (response.ok) {
+          console.log("Usuário registrado com sucesso!")
+          console.log(email, username)
+        } else {
+          console.error("Erro ao registrar usuário.")
+        }
+      }else{
+        window.alert("As senhas devem ser iguais, tente novamente.")
+      }
+    } catch (error) {
+      console.error("Erro ao registrar usuário.", error)
+    }
+  }
+
+  const [page, setPage] = useState("1")
 
   const handleChange = (_event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue)
+    setPage(newValue)
   }
 
   return (
     <>
-      <TabContext value={value}>
+      <TabContext value={page}>
         <div id="formContainer">
           <Box sx={{ width: "fit-content", marginLeft: "35px" }}>
             <TabList sx={{ boxShadow: "-10 0 5 black" }} onChange={handleChange} aria-label="lab API tabs example">
@@ -39,19 +73,39 @@ export const RegisterForm = () => {
           <TabPanel value="2">
             <form id="register-form" className="user-form">
               <div className="form-section">
-                <input type="email" placeholder="E-mail" />
+                <input 
+                  type="email" 
+                  placeholder="E-mail" 
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
               </div>
               <div className="form-section">
-                <input type="text" placeholder="Nome" />
+                <input
+                  type="text"
+                  placeholder="Nome"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
               </div>
               <div className="form-section">
-                <input type="password" placeholder="Senha" />
+                <input
+                  type="password"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value) }
+                />
               </div>
               <div className="form-section">
-                <input type="password" placeholder="Repita a senha" />
+                <input
+                  type="password"
+                  placeholder="Repita a senha"
+                  value={repassword}
+                  onChange={(e) => setRepassword(e.target.value)}
+                />
               </div>
               <div className="form-section">
-                <button type="submit">Button</button>
+                <button onClick={(event) => handleRegister(event)} type="submit">Button</button>
                 <p>forgot your password?</p>
               </div>
             </form>
